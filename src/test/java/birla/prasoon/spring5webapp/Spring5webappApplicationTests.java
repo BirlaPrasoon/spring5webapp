fe49bcd9be0d33240af70d39f82413d6968869c0
Prasoon Birla <prasoon.birla@whatfix.com>
2020-08-05 14:55:08 +0530

    First commit

---
+++ src/test/java/birla/prasoon/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,12 @@
+package birla.prasoon.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
